ad9a15ebf44b6914e82e5db82fd27e8c1833e56f
akrooss <akrooss@users.noreply.github.com>
2025-08-26 15:11:10 +0000

    deploy: 1dfd13f27af30c59334d849cfbe961eb4dd306f1

---
+++ _next/static/_2PpPcermFhSoZ2DKR1v8/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/9bcf7f6c945b902a.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/mein-forschungsprojekt//_next/:path+",
+        "destination": "/mein-forschungsprojekt/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/ea4c0f8ad09ecd8f.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/8c5b045c0eecb3ec.js",
+  "static/chunks/turbopack-e89f369adfb8f6d1.js"
+])
