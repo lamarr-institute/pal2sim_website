ed832dbe2a4ea7cf651f284d0a9cadf6893952c6
akrooss <akrooss@users.noreply.github.com>
2026-01-12 14:59:15 +0000

    deploy: a98ddf9e0132a2ad393e1f9aa8019f59d6fcbbe3

---
+++ _next/static/SBH1B7DqGxGX0ovLImwAu/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/396fc161fdb6ca76.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/pal2sim_website//_next/:path+",
+        "destination": "/pal2sim_website/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
